068675b5b692350cb44a4219e8a73fffcfa93ee6
Kuai-sama <77694138+Kuai-sama@users.noreply.github.com>
2021-01-22 02:06:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 /*
    En fonction d'une date rentrée par l'utilisateur, ce programme permet d'obtenir le jour de ladite date
 */
-#include <iostream>  //inclusion de la bibliothèque "iostream"(gère les flux entrées/sorties)
-#include <windows.h> //inclusion du fichier d'en-tête "windows.h"(afficher les caractères spéciaux)
-using namespace std;//simplifie l'ecriturte du code, utilise le nom d'espace standard(enleve les std::)
+#include <iostream>   //Inclusion de la bibliothèque "iostream"(gère les flux entrées/sorties)
+#include <windows.h> //Inclusion du fichier d'en-tête "windows.h"(afficher les caractères spéciaux)
+using namespace std;//Simplifie l'ecriturte du code, utilise le nom d'espace standard(enleve les std::)
 
-int main()      //fonction principale
+int main()
 {
-    unsigned int jour=0, mois=0, anee,debutAnee=0, finAnee=0;//déclaration et initialisation des variables
-    SetConsoleOutputCP(CP_UTF8);                            //permet d'afficher les caractères spéciaux
+    unsigned int jour=0, mois=0, annee,debutAnnee=0, finAnnee=0;//Déclaration et initialisation des variables
+    SetConsoleOutputCP(CP_UTF8);                            //Permet d'afficher les caractères spéciaux
 
     cout << "                       Formule de Zeller" << endl;
     cout << "Permet de déterminer le jour en fonction d'une date donée" << endl;
@@ -23,24 +23,24 @@ int main()      //fonction principale
     cin >>mois;
 
     cout << "Saississez un nombre pour l'anée : ";
-    cin >>anee;//rentre l'information de l'anée via le clavier
+    cin >>annee;
 
     if(mois==1)//Si le mois est égal à 1(janvier)
     {
         mois=13;//Le mois devient égal à 13
     }
 
-    if(mois==2)//si le mois est égal à 2(février)
+    if(mois==2)//Si le mois est égal à 2(février)
     {
         mois=14;//Le mois devient égal à 14
     }
 
-    debutAnee=(anee/100);//On divise par 100 pour obtenir les deux premiers chiffre de l'anée
+    debutAnnee=(annee/100);//On divise par 100 pour obtenir les deux premiers chiffre de l'année
 
-    finAnee=(anee-(debutAnee*100));//soustrait le nombre de départ au nombre rond, pour obtenir les deux derniers chiffre de l'anée
+    finAnnee=(annee-(debutAn,ee*100));//Soustrait le nombre de départ au nombre rond, pour obtenir les deux derniers chiffre de l'année
 
-    int resultat= jour+(2.6*(mois+1))+(debutAnee/4)+(5*debutAnee+finAnee)+(finAnee/4);//calcul via la formule de zeller
-    unsigned int  reste=resultat%7;//On cherche le reste du resultat divisé par 7, pour trouver le jour correspondant
+    int resulta t= jour+(2.6*(mois+1))+(debutAnnee/4)+(5*debutAnnee+finAnnee)+(finAnnee/4);//Calcul via la formule de zeller
+    unsigned int reste=resultat%7;//On cherche le reste du resultat divisé par 7, pour trouver le jour correspondant
 
   switch(reste)//selon le reste
   {
